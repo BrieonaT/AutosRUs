ab077fafc0703019a00e1db3d2ded7acd67f71da
BrieonaT <84942184+BrieonaT@users.noreply.github.com>
2021-09-09 15:45:37 -0400

    Updating File for Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+## Deliverable 1 
+
 library(dplyr)
 
 MechCarMPGdf <- read.csv('MechaCar_mpg.csv') #import dataset
@@ -5,3 +7,12 @@ MechCarMPGdf <- read.csv('MechaCar_mpg.csv') #import dataset
 lm(vehicle_length ~ mpg,MechCarMPGdf) #create linear model
 
 summary(lm(vehicle_length ~ mpg,MechCarMPGdf)) #summarize linear model
+
+
+## Deliverable 2
+
+SuspensionCoilTable <- read.table(file='Suspension_Coil.csv', header = TRUE, sep = ",") #import 
+
+total_summary <- SuspensionCoilTable %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table
+
+lot_summary <- SuspensionCoilTable %>% group_by(Manufacturing_Lot) %>%  summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) #create summary table for lot
